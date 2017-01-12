40e9e127f83f3dda00b547e0cf9db305e5ab0ca9
Yorick <ephase@xieme-art.org>
2017-01-13 00:26:25 +0100

    jpeg processing use exiv-py now

--- processors.py
+++ processors.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python
 import os
-#import subprocess
-#import shlex
-from gi.repository import GExiv2
+# import subprocess
+# import shlex
+from exifpy import exifread
 from mutagenx.easyid3 import EasyID3
-from datetime import date, datetime
+from datetime import datetime
 
 
 def process_default(f):
-    return os.path.splitext(f)[1][1:],os.path.splitext(os.path.split(f)[1])[0]
+    return os.path.splitext(f)[1][1:], os.path.splitext(os.path.split(f)[1])[0]
+
 
 def process_jpg(f):
-    p = GExiv2.Metadata(f)
-    try: 
-        exivDate = datetime.strptime(p['Exif.Photo.DateTimeOriginal'], '%Y:%m:%d %H:%M:%S')
+    print("processing jpeg File")
+    try:
+        b = open(f, 'rb')
+        p = exifread.process_file(b, stop_tag='EXIF DateTimeOriginal')
+        exivDate = datetime.strptime(p['EXIF DateTimeOriginal'].printable,
+                                     '%Y:%m:%d %H:%M:%S')
+        print(exivDate)
         folder = 'jpg/'+exivDate.strftime('%Y')+'/'+exivDate.strftime('%m')
         name = exivDate.strftime('%Y')+exivDate.strftime('%m')+exivDate.strftime('%d')\
-            + "_" + exivDate.strftime('%H') + exivDate.strftime('%M') + exivDate.strftime('%S')
-        return folder,name
+            + "_" + exivDate.strftime('%H') + exivDate.strftime('%M')\
+            + exivDate.strftime('%S')
+        return folder, name
     except:
         return process_default(f)
 
+
 def process_mp3(f):
-    try:
+    try: 
         tag = EasyID3(f)
     except:
         return 'mp3/','unknow'
